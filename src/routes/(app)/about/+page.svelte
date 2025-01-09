6a7de26f365504e99ddd429cab692d9ac46eaa74
edwardhuynh <edwardleehuynh@gmail.com>
2025-01-09 16:18:57 +0800

    more photosnpm run dev

--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -12,7 +12,7 @@
   margin-top: 12px;
   border: solid, 2px, #B9BBB6;
   border-radius: 10px;
-  padding: 8px;
+  padding: 0.5%;
 }
 
 img {
@@ -21,26 +21,76 @@ img {
   margin-right: auto;
   max-width: 100%;
 }
+
+.threeframe {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  height: auto;
+  justify-content: center;
+
+
+}
+.threeframe img {
+  object-fit: contain;
+  width: 32%;
+  max-height: 100%;
+  height: auto;
+}
+
+.threeframe p {
+  width: 30%;
+  margin: 1%;
+}
+
+.twoframe {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  height: 66%;
+  justify-content: center;
+}
+
+.twoframe img {
+  object-fit: contain;
+  width: 32%;
+  max-height: 100%;
+  height: auto;
+}
+
+.twoframe div {
+  width: 16%;
+}
+
+.twoframe p {
+  width: 30%;
+  margin: 1%;
+}
 </style>
 
 <h3>Hi again!</h3>
 
 <p>As I've said before, I graduated from Perth Modern School in 2024. In my final year, I studied Mathematics Methods and Specialist, Physics, Chinese Second Language and Literature. I accelerated Chemistry, and so I sat the 2023 WACE exam for Chemistry. Ultimately, I ended up with an ATAR of 99.95 and a Subject Exhibition in Mathematics Specialist.</p>
 
-<div class="frame">
-  <img src="/images/meandwidj.png" alt="Left to right: Me and Ethan Widjaja">
+<div class="threeframe">
+  <img src="/images/meandwidj.png" alt="Left to right: Me and Ethan Widjaja at our graduation :)">
+  <img src="/images/meandbigb.jpg" alt="Left to right: Me and Pranav Borude being very confused...">
+  <img src="/images/itsmygoat.jpg" alt="Left to right: Ethan Yap and me (he's very cool)">
   <p class="caption">Left to right: Me and Ethan Widjaja at our graduation :)</p>
+  <p class="caption">Left to right: Me and Pranav Borude being very confused...</p>
+  <p class="caption">Left to right: Ethan Yap and me (he's very cool)</p>
 </div>
 
 <p>I'm looking forward to studying biomedicine in university, at any uni that wants to take me ;)</p>
-<p>In my spare time, I like to play piano and make silly coding projects which you can find on my <a href="https://github.com/EddieTheEd">Github</a>. I also like to play badminton with my friends.</p>
+<p>In my spare time, I like to play piano and make silly coding projects which you can find on my <a href="https://github.com/EddieTheEd">Github</a>. I also like to play badminton with my friends, and I used to play chess for my school, representing Perth Modern School at interstate events. If you came up to me at school in year 12, you'd probably find me playing chess with Pranav on our favourite bench ❤.</p>
 
-<div class="frame">
+<div class="twoframe">
+  <div></div>
   <img src="/images/baddy.png" alt="Me waving at someone...?">
+  <img src="/images/shahmat.jpg" alt="Chess!">
+  <div></div>
+  <div></div>
   <p class="caption">Me waving at someone...?</p>
-</div>
-
-<div class="frame">
-  <img src="/images/meandbigb.jpg" alt="Left to right: Me and Pranav Borude being confused by the photographer's instructions...">
-  <p class="caption">Left to right: Me and Pranav Borude being confused by the photographer's instructions...</p>
+  <p class="caption">Chess on a bench!</p>
+  <div></div>
 </div>
