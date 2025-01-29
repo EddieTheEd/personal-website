714e563a88de3216765a43353e4066d5e4dd4b4e
edwardhuynh <edwardleehuynh@gmail.com>
2025-01-29 19:12:18 +0800

    write better

--- src/routes/(app)/writings/[slug]/+page.svelte
+++ src/routes/(app)/writings/[slug]/+page.svelte
@@ -2,8 +2,27 @@
 	export let data;
 </script>
 
+<style>
+  .writingheader {
+    display: flex;
+    justify-content: space-between;
+    align-items: center; /* Ensures both elements are aligned on the same baseline */
+  }
+  .writingheader h2 {
+    text-decoration: underline;
+    margin: 0;
+  }
+  .writingheader p {
+    font-style: italic;
+    color: #909090; 
+    margin: 0;
+  }
+</style>
+
 <article>
-	<h2>{data.title}</h2>
-	<p>Published: {data.date}</p>
+  <div class='writingheader'>
+    <h2>{data.title}</h2>
+    <p>Published: {data.date}</p>
+  </div>
 	<svelte:component this={data.content} />
 </article>
