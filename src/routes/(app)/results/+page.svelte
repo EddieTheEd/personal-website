f7a27afd311e24eb07fe7e1bb9829bcdc0c8551b
edwardhuynh <edwardleehuynh@gmail.com>
2025-03-12 19:57:25 +0800

    done

--- src/routes/(app)/results/+page.svelte
+++ src/routes/(app)/results/+page.svelte
@@ -1,4 +1,4 @@
-<p>Ultimately, I ended up with an ATAR of 99.95. However, I think this number can be a bit deceptive and fails to present the full picture. So to be transparent, I'll share with you my school and WACE results for all the subjects I took (except for Literature, because I did really badly 🫠)</p>
+<p>I ended up with an ATAR of 99.95. See my WACE results below (excluding literature)!</p>
 
 <style>
   table, th, td {
@@ -20,7 +20,6 @@
   <thead>
     <tr>
       <th></th>
-      <th>School Mark</th>
       <th>WACE Mark</th>
       <th>Scaled Mark</th>
     </tr>
@@ -28,37 +27,31 @@
   <tbody>
     <tr>
       <td class="subject">Mathematics Specialist</td>
-      <td>90.00</td>
       <td>98.47</td>
       <td>98.8</td>
     </tr>
     <tr>
       <td class="subject">Chemistry (2023)</td>
-      <td>89.00</td>
       <td>90.20</td>
       <td>93.1</td>
     </tr>
     <tr>
       <td class="subject">Mathematics Methods</td>
-      <td>96.00</td>
       <td>91.44</td>
       <td>92.3</td>
     </tr>
     <tr>
       <td class="subject">Physics</td>
-      <td>91.00</td>
       <td>92.57</td>
       <td>91.7</td>
     </tr>
     <tr>
       <td class="subject">Chinese Second Language (written, oral)</td>
-      <td>83.00, 91.00</td>
       <td>94.19, 95.83</td>
       <td>87.3</td>
     </tr>
   </tbody>
 </table>
 
-<p>I ended up with a TEA of 403.7. Not that great, and now you can see why!</p>
 <p>While I may have done quite well in Specialist and Chinese (WACE-wise), Methods and Physics were quite a big let-down. When I get those exams back, I'll be sure to discuss where I went wrong here!</p>
 <p>In terms of SCSA's awards, I was awarded the Subject Exhibition (highest mark in WACE) and Certificate of Excellence (top 0.5% WACE mark) in Mathematics Specialist. I also achieved the Certificate of Distinction (getting A's for year 11 and 12 in 5 subjects).</p>
