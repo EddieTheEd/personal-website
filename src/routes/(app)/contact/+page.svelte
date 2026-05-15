62f8c3a47f4fb065ddce008fa27c62b6f0701b02
edwardhuynh <edwardleehuynh@gmail.com>
2026-05-15 23:55:02 +1000

    update to domain email, will stop using toomwn domain

--- src/routes/(app)/contact/+page.svelte
+++ src/routes/(app)/contact/+page.svelte
@@ -1,6 +1,6 @@
 
 <ul>
-  <li>Email: <a href="mailto:ed@toomwn.xyz">ed@toomwn.xyz</a></li>
+  <li>Email: <a href="mailto:ed@elh.id.au">ed@elh.id.au</a></li>
   <li>Github: <a href="https://github.com/EddieTheEd">EddieTheEd</a></li>
   <li>Discord: <i>definitelyeddie.</i></li>
 </ul>
